604689351183dd67a01d2769bfe3a8a501dda659
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-09 20:59:07 -0300

    refactor: remove unused controllers

--- src/controllers/HelloWorldController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-class HelloWorldController {
-  index(request: Request, response: Response) {
-    return response.status(200).json({ message: "Hello world!" });
-  }
-}
-
-export { HelloWorldController };
