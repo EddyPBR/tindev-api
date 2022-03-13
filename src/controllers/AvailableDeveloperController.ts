378f394e9188b98bae37408147b3009d627a9119
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-13 11:00:56 -0300

    feat: update AvailableDeveloperController to not use service unecessary

--- src/controllers/AvailableDeveloperController.ts
+++ src/controllers/AvailableDeveloperController.ts
@@ -1,27 +1,16 @@
 import { Request, Response } from "express";
 import { AvailableDeveloperService } from "@services/AvailableDeveloperService";
-import { FindDeveloperByIdService } from "@services/FindDeveloperByIdService";
 
 class AvailableDeveloperController {
   async handle(request: Request, response: Response) {
-    const id = request.headers.id as string;
-
-    const findDeveloperByIdService = new FindDeveloperByIdService().execute;
-
-    const developerExists = await findDeveloperByIdService({
-      developerId: id,
-    });
-
-    if (!developerExists) {
-      return response.status(403).json({ message: "user not authenticated!" });
-    }
+    const developer = request.developer;
 
     const availableDeveloperService = new AvailableDeveloperService().execute;
 
     const developers = await availableDeveloperService({
-      developerId: developerExists.id,
-      likes: developerExists.likes.map((like) => like.developerId),
-      dislikes: developerExists.likes.map((dislike) => dislike.developerId),
+      developerId: developer.id,
+      likes: developer.likes.map((like) => like.developerId),
+      dislikes: developer.likes.map((dislike) => dislike.developerId),
     });
 
     return response.json(developers);
