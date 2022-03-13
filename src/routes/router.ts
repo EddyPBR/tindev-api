cf1f5bc1b3ec6412e91960d726dbb1d4b251cbf3
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-13 10:59:27 -0300

    refactor: update route [GET] "/developers"

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { AvailableDeveloperController } from "@controllers/AvailableDeveloperController";
 import { CreateDeveloperController } from "@controllers/CreateDeveloperController";
+import { ensureAuthenticated } from "@middlewares/ensureAuthenticated";
 
 const router = Router();
 
-router.get("/developers", new AvailableDeveloperController().handle);
+router.get("/developers", ensureAuthenticated, new AvailableDeveloperController().handle);
 router.post("/developers", new CreateDeveloperController().handle);
 
 export { router };
